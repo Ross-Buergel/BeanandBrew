ce36fd956557954c4d6d182a05402239d6941d24
Ross Buergel <ross_buergel@hotmail.com>
2023-03-17 15:08:13 +0000

    began updating includes

--- includes/connect_db.php
+++ includes/connect_db.php
@@ -1,14 +1,18 @@
 <?php
+//assigns appropriate values to variables
 $host = "localhost";
 $dbname = "db_beanandbrew";
 $username = "root";
 $password = "";
 
+//creates database connection
 $dbc = new mysqli($host, $username, $password, $dbname);
 
+//outputs error if one occurs
 if ($dbc->connect_errno) {
     die("Connection error: " . $dbc->connect_error);
 }
 
+//returns connection
 return $dbc;
 ?>
--- includes/validation-functions.php
+++ includes/validation-functions.php
@@ -1,32 +1,41 @@
 <?php
+//checks length of a variable 
 function check_length($variable_name, $variable, $min_length, $max_length) {
+    //returns message if too short
     if ($variable < $min_length){
         return $variable_name." cannot be less than ".$min_length." characters";
     }
+    //returns message if too long
     else if ($variable < $max_length){
         return $variable_name." cannot be more than ".$max_length." characters";
     }
 }
 
+//checks a variable has been entered
 function check_presence($variable_name, $variable){
+    //returns message if empty
     if (empty($variable)){
         return $variable_name." is requred";
     }
 }
 
+//checks whether a variable contains an integer
 function check_contains_integer($variable_name, $variable)
 {
     //splits variable into characters
     $characters = str_split($variable);
-
+    
+    //defines necessary variable
     $contains_digit = False;
 
+    //loops through each character and sets contains_digit to True if character is an integer
     foreach ($characters as $character) {
         if (is_int($character)) {
             $contains_digit = True;
         }
     }
 
+    //checks if variable contained an integer and if so returns appropriate message
     if (isset($contains_digit) && $contains_digit == True)
     {
         return $variable_name . " cannot contain an integer";
